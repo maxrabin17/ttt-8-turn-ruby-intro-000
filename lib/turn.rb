2d0cf7c0d42baf677827602693e191e18f766420
Max Rabin <max.rabin17@gmail.com>
2021-02-22 20:27:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,9 +31,6 @@ def move(board, index, token = "X")
   board[index] = token
 end
 
-def current_player(board)
-  turn_count(board) % 2 == 0? "X" : "O"
-
 def turn(board)
   puts "Please enter 1-9:"
 end
