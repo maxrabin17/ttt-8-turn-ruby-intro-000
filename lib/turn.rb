fde61b108d82ddba79d8551b9baf9c8e2e197fef
Max Rabin <max.rabin17@gmail.com>
2021-02-22 16:01:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index
+  
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
+end
+
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0, 8)
     return true
